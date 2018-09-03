f50d5b5f66e753168754c07c7c927fc069af2c10
MiranteDev <41705897+MiranteDev@users.noreply.github.com>
2018-09-03 22:53:59 +0100

    Ver recibo

--- verrecibo.php
+++ verrecibo.php
@@ -4,6 +4,11 @@
     <h2>Recibos</h2>
     <hr>
     <div class="row">
+      <div class="col">
+      <div class="card">
+        <h2>Filtros</h2>
+          <div class="card-body">
+            <div class="row">
       <div class="col-lg-4">
         <div class="input-group mb-3">
   <div class="input-group-prepend">
@@ -29,47 +34,79 @@
 <button type="button" class="btn btn-info btn-block" name="button">Pesquisar</button>
 </div>
   </div>
+</div>
+</div>
+</div>
+</div>
   <hr>
+  <div class="row">
+    <div class="col">
+      <div class="card">
+          <div class="card-body">
+              <div class="row">
+            <div class="col">
+                <h1>Joao Mirante</h1>
+            </div>
+
+
+              <div class="col" >
+                  <h2 style="text-align:right;">Agosto 2015</h2>
+              </div>
+            </div>
+            <hr>
+              <h3>NIF: 26045564</h3>
+
+
+
+        </div>
+      </div>
+  </div>
+</div>
     <div class="row">
 
-        <div class="col-lg-4">
+        <div class="col">
           <div class="card">
               <div class="card-body">
                 </div>
+                <div class="table-responsive">
+                                    <table class="table">
+                                        <thead>
+                                            <tr>
+                                                <th>#</th>
+                                                <th>Name</th>
+                                                <th>Status</th>
+                                                <th>Date</th>
+                                                <th>Price</th>
+                                            </tr>
+                                        </thead>
+                                        <tbody>
+                                            <tr>
+                                                <th scope="row">1</th>
+                                                <td>Kolor Tea Shirt For Man</td>
+                                                <td><span class="badge badge-primary">Sale</span></td>
+                                                <td>January 22</td>
+                                                <td class="color-primary">$21.56</td>
+                                            </tr>
+                                            <tr>
+                                                <th scope="row">2</th>
+                                                <td>Kolor Tea Shirt For Women</td>
+                                                <td><span class="badge badge-success">Tax</span></td>
+                                                <td>January 30</td>
+                                                <td class="color-success">$55.32</td>
+                                            </tr>
+                                            <tr>
+                                                <th scope="row">3</th>
+                                                <td>Blue Backpack For Baby</td>
+                                                <td><span class="badge badge-danger">Extended</span></td>
+                                                <td>January 25</td>
+                                                <td class="color-danger">$14.85</td>
+                                            </tr>
+                                        </tbody>
+                                    </table>
+                                </div>
                 </div>
         </div>
-        <div class="col-lg-4">
-          <div class="card">
-              <div class="card-body">
-                </div>
-                </div>
-        </div>
-        <div class="col-lg-4">
-          <div class="card">
-              <div class="card-body">
-                </div>
-                </div>
-        </div>
-    </div>
-    <div class="row">
-        <div class="col-lg-4">
-          <div class="card">
-              <div class="card-body">
-                </div>
-                </div>
-        </div>
-        <div class="col-lg-4">
-          <div class="card">
-              <div class="card-body">
-                </div>
-                </div>
-        </div>
-        <div class="col-lg-4">
-          <div class="card">
-              <div class="card-body">
-                </div>
-                </div>
-        </div>
+
     </div>
 </div>
 </div>
