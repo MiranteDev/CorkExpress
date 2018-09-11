6a8692f792b27ff2c1ee5ed82fc4c1d1e372a9b7
Joaopedroteixeira <jonhdrift@gmail.com>
2018-09-11 18:46:26 +0100

    Novas coisas

--- indexadmin.php
+++ indexadmin.php
@@ -86,7 +86,7 @@ $(document).ready(function(){
             <nav class="navbar top-navbar navbar-expand-md navbar-light">
                 <!-- Logo -->
                 <div class="navbar-header">
-                    <a class="navbar-brand" href="indexadmin.html">
+                    <a class="navbar-brand" href="index.php">
                         <!-- Logo icon -->
                         <b><img src="images/logo.png" alt="homepage" class="dark-logo" /></b>
                         <!--End Logo icon -->
